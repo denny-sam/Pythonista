8c77f21a316ffc1e353d50ee0ea0a8635b016b35
Denny P. Sam <blackstorm.denny@gmail.com>
2015-09-28 01:32:55 +0530

    Create thenewboston-crawler.py

---
+++ thenewboston-crawler.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_points(url):
+    r=requests.get(url)
+    x= BeautifulSoup(r.text,'html.parser')
+    user=x.findAll('span',{'class':'titles'});
+
+    for f in x.findAll('td'):
+        for n in f.findAll('span'):
+            print(user[0].text,':',n.text)
+
+for q in range(2,40):
+    get_points("https://www.thenewboston.com/profile.php?user="+str(q))
